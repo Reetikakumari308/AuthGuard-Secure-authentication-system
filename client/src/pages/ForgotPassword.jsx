bd6574dfdf7b7b3c5469d4e6a359ec275e1620dd
Reetikakumari308 <98039204+Reetikakumari308@users.noreply.github.com>
2025-05-27 23:55:09 +0530

    Update ForgotPassword.jsx

--- client/src/pages/ForgotPassword.jsx
+++ client/src/pages/ForgotPassword.jsx
@@ -12,7 +12,7 @@ const ForgotPassword = () => {
     e.preventDefault();
     await axios
       .post(
-        "http://localhost:4000/api/v1/user/password/forgot",
+        "https://authguard-secure-authentication-system.onrender.com/api/v1/user/password/forgot",
         { email },
         {
           withCredentials: true,
