7a06d5d853524f974c017c141688e644f730bee3
Reetikakumari308 <98039204+Reetikakumari308@users.noreply.github.com>
2025-05-28 02:20:10 +0530

    Update Auth.jsx

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -8,32 +8,33 @@ import Login from "../components/Login";
 const Auth = () => {
   const { isAuthenticated } = useContext(Context);
   const [isLogin, setIsLogin] = useState(true);
+
   if (isAuthenticated) {
-    return <Navigate to={"/"} />;
+    return <Navigate to="/" />;
   }
+
   return (
-    <>
-      <div className="auth-page">
-        <div className="auth-container">
-          <div className="auth-toggle">
-            <button
-              className={`toggle-btn ${isLogin ? "active" : ""}`}
-              onClick={() => setIsLogin(true)}
-            >
-              Login
-            </button>
-            <button
-              className={`toggle-btn ${!isLogin ? "active" : ""}`}
-              onClick={() => setIsLogin(false)}
-            >
-              Register
-            </button>
-          </div>
-          {isLogin ? <Login /> : <Register />}
+    <div className="auth-page">
+      <div className="auth-container">
+        <div className="auth-toggle">
+          <button
+            className={`toggle-btn ${isLogin ? "active" : ""}`}
+            onClick={() => setIsLogin(true)}
+          >
+            Login
+          </button>
+          <button
+            className={`toggle-btn ${!isLogin ? "active" : ""}`}
+            onClick={() => setIsLogin(false)}
+          >
+            Register
+          </button>
         </div>
+        {isLogin ? <Login /> : <Register />}
       </div>
-    </>
+    </div>
   );
 };
 
 export default Auth;
+
