121543d659b8634bd3cedd9284eb20630d129304
Reetikakumari308 <98039204+Reetikakumari308@users.noreply.github.com>
2025-05-28 02:22:06 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,48 +1,16 @@
-import React, { useContext } from "react";
-import Hero from "../components/Hero";
-import Instructor from "../components/Instructor";
-import Technologies from "../components/Technologies";
-import "../styles/Home.css";
-import { toast } from "react-toastify";
-import axios from "axios";
-import { Context } from "../main";
-import { Navigate, useNavigate } from "react-router-dom";
-import Footer from "../layout/Footer";
-
-const Home = () => {
-  const { isAuthenticated, setIsAuthenticated, setUser } = useContext(Context);
-
-  const logout = async () => {
-    await axios
-      .get("https://authguard-secure-authentication-system.onrender.com", {
-        withCredentials: true,
-      })
-      .then((res) => {
-        toast.success(res.data.message);
-        setUser(null);
-        setIsAuthenticated(false);
-      })
-      .catch((err) => {
-        toast.error(err.response.data.message);
-        console.error(err);
-      });
-  };
-
-  if (!isAuthenticated) {
-    return <Navigate to={"/auth"} />;
-  }
-
-  return (
-    <>
-      <section className="home">
-        <Hero />
-        <Instructor />
-        <Technologies />
-        <Footer />
-        <button onClick={logout}>Logout</button>
-      </section>
-    </>
-  );
+const logout = async () => {
+  await axios
+    .get(`${process.env.REACT_APP_BACKEND_URL}/api/v1/user/logout`, {
+      withCredentials: true,
+    })
+    .then((res) => {
+      toast.success(res.data.message);
+      setUser(null);
+      setIsAuthenticated(false);
+    })
+    .catch((err) => {
+      toast.error(err.response?.data?.message || "Logout failed");
+      console.error(err);
+    });
 };
 
-export default Home;
