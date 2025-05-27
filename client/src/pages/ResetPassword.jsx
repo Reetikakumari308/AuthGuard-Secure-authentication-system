a00d7646da82eee2547d3849b0343aa80800fac2
Reetikakumari308 <98039204+Reetikakumari308@users.noreply.github.com>
2025-05-27 23:58:00 +0530

    Update ResetPassword.jsx

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -15,7 +15,7 @@ const ResetPassword = () => {
     e.preventDefault();
     await axios
       .put(
-        `http://localhost:4000/api/v1/user/password/reset/${token}`,
+        `https://authguard-secure-authentication-system.onrender.com/api/v1/user/password/reset/${token}`,
         { password, confirmPassword },
         {
           withCredentials: true,
